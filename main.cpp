77ed9e706f106caadd90209f6a9570aaf18ec6d3
Gandi <kuba.wasielak@gmail.com>
2011-04-23 20:44:23 +0200

    * Added main instuctions.

--- main.cpp
+++ main.cpp
@@ -5,4 +5,24 @@
  *      Author: Gandi
  */
 
-//Testing.
+/*
+ * Kilka sugestii co do tworzenia przez nas projektu.
+ *
+ * 1. Wszystkie commity komentujcie krótko i tresciwie.
+ *
+ * 2. Jesli zaczynacie tworzyc nowa klase lub funkcje, to pierwsze co powinno sie zrobic,
+ * to dodanie komentarza na temat co ta klasa/metoda ma robic i co pobierac.
+ *
+ * 3. Unikajmy nazw tmp=1, a=cos, dupa1==dupa2. Mam nadzieje, ze rozumiecie o co mi chodzi ;)
+ *
+ * 4. Jak najwiecej rzeczy, ktore sie da, piszmy w klasach. Latwiej jest z pliku .h importowac
+ * klase, stworzyc jej instancje i dzialac juz na jej metodach, zamiast tworzyc setke funkcji,
+ * a potem sie w nich gubic. Bo bedziemy sie gubic przy tylu osobach, tego nie da sie ukryc.
+ *
+ * 5. Dzielmy pliki na mniejsze. Zamiast stworzyc plik o 1000 linijek, lepiej stworzyc 4 po 250
+ * (o ile sie da oczywiscie).
+ *
+ * 6. Z czym sie nie zgadzacie lub co widzicie inaczej - piszcie.
+ *
+ *
+ */
